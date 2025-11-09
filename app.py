b59871e8ccdfdf023c1b12ce2e44184a67121ec7
vishwajeetyadavv <vishwajeet.yadav@navgrahaa.com>
2025-11-09 12:45:31 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,33 +1,23 @@
 import streamlit as st
-import streamlit_authenticator as stauth
-import yaml
 from streamlit_authenticator import Authenticate
-# # Load secrets
-# secrets = st.secrets["credentials"]
-# auth_config = st.secrets["authenticator"]
+from copy import deepcopy
 
-# credentials = {
-#     "usernames": secrets["usernames"]
-# }
+# Read from secrets (read-only objects)
+_secrets_credentials = st.secrets["credentials"]
+_secrets_cookie = st.secrets["cookie"]
 
-# # Initialize authenticator
-# authenticator = stauth.Authenticate(
-#     credentials,
-#     auth_config["cookie_name"],
-#     auth_config["cookie_key"],
-#     auth_config["expiry_days"]
-# )
+# Make mutable copies
+credentials = deepcopy(_secrets_credentials)
+cookie = deepcopy(_secrets_cookie)
 
-
-credentials = st.secrets["credentials"]
-cookie = st.secrets["cookie"]
-
+# Build authenticator
 authenticator = Authenticate(
     credentials=credentials,
     cookie_name=cookie["name"],
     key=cookie["key"],
     cookie_expiry_days=int(cookie["expiry_days"]),
 )
+
 # Login widget
 name, authentication_status, username = authenticator.login("Login", "main")
 
@@ -38,5 +28,5 @@ if authentication_status:
     st.write("This page is protected!")
 elif authentication_status is False:
     st.error("Username or password incorrect")
-elif authentication_status is None:
+else:
     st.warning("Please log in to continue.")
